0983ffaf905f75690d9cda2318840785c0aabff7
Andrejsko2 <gobex7@gmail.com>
2020-04-12 16:49:36 +0200

    Window

--- PetitPaint/Main.cpp
+++ PetitPaint/Main.cpp
@@ -1,10 +1,36 @@
 #include<iostream>
+#include<SDL.h>
 
 using namespace std;
 
-int main()
+const int SCREEN_HEIGHT=500;
+const int SCREEN_WIDTH=500;
+
+int main(int argc, char* argv[])
 {
-    cout<<"Hello newest\n";
+    SDL_Window* window=nullptr;
+    SDL_Surface* screenSurface=nullptr;
+
+    if(SDL_Init(SDL_INIT_VIDEO)<0)
+    {
+        cout<<"SDL could not initialize. Error: "<<SDL_GetError()<<"\n";
+        return 0;
+    }
+    window = SDL_CreateWindow( "PetitPaint", SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if(window==nullptr)
+    {
+        cout<<"SDL window not created. Error: "<<SDL_GetError()<<"\n";
+        return 0;
+    }
+
+    screenSurface=SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0x0, 0x0, 0x0));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
 
     return 0;
 }
